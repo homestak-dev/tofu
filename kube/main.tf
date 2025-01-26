33aab24f52279a9aed9460e60dc98e4decad56ed
John DeRose <jderose@jderose.net>
2025-01-26 16:35:03 -0700

    refactored (again) to use "map of maps" spec for provisioning.

--- kube/main.tf
+++ kube/main.tf
@@ -11,93 +11,52 @@ module "cloud_image" {
   source = "../proxmox-file"
 }
 
-module "k3s_mgmt" {
-  source = "../proxmox-vm"
-  count  = 3      # set this >= 1 to enable, 0 to disable
+locals {
+  clusters = {
+    demo = {                # cluster name (e.g., mgmt, prod, etc.)
+      count     = 1         # number of instances; set >= 1 to enable, 0 to disable
+      bridge    = "vmbr0"
+      hostname  = "tofutest"
+    },
+    sysadm = {
+      count     = 1
+      bridge    = "mgmt"
+      hostname  = "sysadm"
+    },
+    kubeadm = {
+      count     = 3
+      bridge    = "mgmt"
+      hostname  = "kubeadm"
+    },
+    kube-prod = {
+      count     = 3
+      bridge    = "prod"
+      hostname  = "k3s-prod"
+    }
+  }
 
-  # required values
-  cloud_image_id  = module.cloud_image.file_id
-  vm_name         = "mgmt${count.index+1}.kube"
-  vm_hostname     = "mgmt${count.index+1}"
-
-  # optional values
-  vm_tags         = ["k3s","mgmt","tofu","ubuntu"]
-  vm_dns_domain   = "kube.jderose.net"
-  vm_dns_servers  = ["10.0.30.1"]
-  vm_ipv4_address = "10.0.30.10${count.index+1}/24"
-  vm_ipv4_gateway = "10.0.30.1"
-  vm_vlan_id      = "30"
+  nodes = flatten([
+    for cluster_name, cluster_data in local.clusters : [
+      for i in range(cluster_data.count) : {
+        cluster_name = cluster_name
+        instance_num = cluster_data.count > 1 ? i + 1 : ""
+        bridge       = cluster_data.bridge
+        hostname     = cluster_data.hostname
+      }
+    ]
+  ])
 }
 
-module "k3s_prod" {
+module "nodes_constructor" {
   source = "../proxmox-vm"
-  count  = 6      # set this >= 1 to enable, 0 to disable
 
-  # required values
+  for_each = {
+    for instance in local.nodes : 
+    "${instance.cluster_name}${instance.instance_num}" => instance
+  }
+
   cloud_image_id  = module.cloud_image.file_id
-  vm_name         = "prod${count.index+1}.kube"
-  vm_hostname     = "prod${count.index+1}"
-
-  # optional values
-  vm_tags         = ["k3s","prod","tofu","ubuntu"]
-  vm_dns_domain   = "kube.jderose.net"
-  vm_dns_servers  = ["10.0.32.1"]
-  vm_ipv4_address = "10.0.32.10${count.index+1}/24"
-  vm_ipv4_gateway = "10.0.32.1"
-  vm_vlan_id      = "32"
+  vm_bridge       = each.value.bridge
+  vm_hostname     = "${each.value.hostname}${each.value.instance_num}"
+  vm_name         = "${each.value.hostname}${each.value.instance_num}"
 }
-
-module "k3s_stg" {
-  source = "../proxmox-vm"
-  count  = 6      # set this >= 1 to enable, 0 to disable
-
-  # required values
-  cloud_image_id  = module.cloud_image.file_id
-  vm_name         = "stg${count.index+1}.kube"
-  vm_hostname     = "stg${count.index+1}"
-
-  # optional values
-  vm_tags         = ["k3s","stg","tofu","ubuntu"]
-  vm_dns_domain   = "kube.jderose.net"
-  vm_dns_servers  = ["10.0.34.1"]
-  vm_ipv4_address = "10.0.34.10${count.index+1}/24"
-  vm_ipv4_gateway = "10.0.34.1"
-  vm_vlan_id      = "34"
-}
-
-module "k3s_test" {
-  source = "../proxmox-vm"
-  count  = 3      # set this >= 1 to enable, 0 to disable
-
-  # required values
-  cloud_image_id  = module.cloud_image.file_id
-  vm_name         = "test${count.index+1}.kube"
-  vm_hostname     = "test${count.index+1}"
-
-  # optional values
-  vm_tags         = ["k3s","test","tofu","ubuntu"]
-  vm_dns_domain   = "kube.jderose.net"
-  vm_dns_servers  = ["10.0.36.1"]
-  vm_ipv4_address = "10.0.36.10${count.index+1}/24"
-  vm_ipv4_gateway = "10.0.36.1"
-  vm_vlan_id      = "36"
-}
-
-module "k3s_dev" {
-  source = "../proxmox-vm"
-  count  = 3      # set this >= 1 to enable, 0 to disable
-
-  # required values
-  cloud_image_id  = module.cloud_image.file_id
-  vm_name         = "dev${count.index+1}.kube"
-  vm_hostname     = "dev${count.index+1}"
-
-  # optional values
-  vm_tags         = ["k3s","dev","tofu","ubuntu"]
-  vm_dns_domain   = "kube.jderose.net"
-  vm_dns_servers  = ["10.0.38.1"]
-  vm_ipv4_address = "10.0.38.10${count.index+1}/24"
-  vm_ipv4_gateway = "10.0.38.1"
-  vm_vlan_id      = "38"
-}
-
