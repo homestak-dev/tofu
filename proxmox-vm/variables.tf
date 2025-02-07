4f31d0dc08dfdaf5203ccef7205920a83355e960
John DeRose <jderose@jderose.net>
2025-02-06 23:37:48 -0700

    added vm_id and vm_mac_address.

--- proxmox-vm/variables.tf
+++ proxmox-vm/variables.tf
@@ -1,11 +1,17 @@
 variable "proxmox_node_name" {
   description = "Proxmox Node Name; e.g.: pve1"
-  default = "pve1"
+  default = "pve"
   type = string
 }
 
 variable "cloud_image_id" {
   description = ""
+  type = string
+}
+
+variable "vm_id" {
+  description = "internal 3-5 digit numeric identifier for the virtual machine (omit for auto-generated); e.g.: 10101"
+  default = null
   type = string
 }
 
@@ -20,13 +26,13 @@ variable "vm_hostname" {
 }
 
 variable "vm_tags" {
-  description = "list of tags for the virtual machine; e.g.: [\"dev\",\"ubuntu\"]"
+  description = "list of tags for the virtual machine; e.g.: ['dev','ubuntu']"
   default = null
   type = list(string)
 }
 
 variable "vm_bridge" {
-  description = "bridge interface for the virtual machine; e.g.: jderose.net"
+  description = "bridge interface for the virtual machine; e.g.: vmbr0, mgmt, etc."
   default = "vmbr0"
   type = string
 }
@@ -38,13 +44,13 @@ variable "vm_dns_domain" {
 }
 
 variable "vm_dns_servers" {
-  description = "list of dns server(s) for the virtual machine; e.g.: [\"10.0.12.1\"]"
+  description = "list of dns server(s) for the virtual machine; e.g.: ['10.0.12.1']"
   default = null
   type = list(string)
 }
 
 variable "vm_ipv4_address" {
-  description = "ipv4 address for the virtual machine; e.g.: dhcp, 10.0.12.100"
+  description = "ipv4 address for the virtual machine; e.g.: dhcp, 10.0.12.100/24"
   default = "dhcp"
   type = string
 }
@@ -55,8 +61,14 @@ variable "vm_ipv4_gateway" {
   type = string
 }
 
+variable "vm_mac_address" {
+  description = "Ethernet MAC address for the virtual machine (omit to auto-generate); e.g.: BC:24:11:CC:68:2A"
+  default = null
+  type = string
+}
+
 variable "vm_packages" {
-  description = "optional packages for the virtual machine; e.g., [\"curl\",\"git\"]"
+  description = "optional packages for the virtual machine; e.g., ['curl','git']"
   default = [ ]
   type = list(string)
 }
